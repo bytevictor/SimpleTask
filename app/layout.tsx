ef6776ff2b33ef9692b40229507e578a3067ac6d
bytevictor <victorgonzarg@gmail.com>
2024-03-28 23:17:54 +0100

    🍱 feat: changed favicon to Check Mark emoji

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
   title: "SimpleTask",
   description: "A simple, fast and light task manager. Created by bytevictor.",
   icons: {
-    icon: './favicon.ico',
+    icon: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>✅</text></svg>",
   },
 };
 
